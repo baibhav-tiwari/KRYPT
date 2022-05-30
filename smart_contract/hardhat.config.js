d48864a1e40f56c2e83acab9276b4a00517747f2
baibhav <baibhavtiwari37@gmail.com>
2022-05-31 00:20:36 +0530

    master

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,10 +1,12 @@
+const { defaultAccounts } = require('ethereum-waffle');
+
 // https://eth-rinkeby.alchemyapi.io/v2/IHSe0hLLlgMqudeh3xGWAk_5HYyo8jeM
 require('@nomiclabs/hardhat-waffle');
 module.exports = {
   solidity: '0.8.0',
   networks: {
     rinkeby: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/IHSe0hLLlgMqudeh3xGWAk_5HYyo8jeM'
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/IHSe0hLLlgMqudeh3xGWAk_5HYyo8jeM',
       accounts: ['1d8d5a1474270c28ac52ab228215c489905ee64647c3463a398023d730999b6a']
     }
   }
