91ebe9cb959f2c643be9137d38880f209c19318f
baibhav <baibhavtiwari37@gmail.com>
2022-06-01 15:20:35 +0530

    master

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -4,3 +4,13 @@ import { contractABI, contractAddress } from '../utils/constants';
 export const TransactionsContext = React.createContext();
 const { ethereum } = window;
 
+const getEthereumContract = () => {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const transactionsContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+    console.log({
+        provider,
+        signer,
+    })
+}
